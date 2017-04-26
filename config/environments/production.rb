8f0fb778569ec0ad3761ec9abe74355b98ad4ef5
Khoi Nguyen <khnaero@gmail.com>
2017-04-26 17:34:01 -0500

    Fix production.rb v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,13 @@ Rails.application.configure do
 
   # Paperclip using Amazon S3 for Heroku
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials:
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION')
+    }
   }
 
   # In production, :host should be set to the actual host of your application.
